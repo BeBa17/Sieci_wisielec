a9d4622fd160bfb06a0c925650c5aab2eb195dc4
Beata <20beata17@gmail.com>
2020-01-24 09:59:08 +0100

    because of sigpipe removing() after receiving -1

--- server.cpp
+++ server.cpp
@@ -73,10 +73,10 @@ void Client::handleEvent(uint32_t events){
             std::string odp(buff.substr(0,count));
             if(odp.substr(0,2) == "-1"){
                 printf("Przegrana ");
-                //this->remove();
-                Client::numberOfPlayers--;
+                this->remove();
+                /*Client::numberOfPlayers--;
                 Client::numberOfPlayersNow--;
-                this->player = false;
+                this->player = false;*/
                 }
             else {
                 if(std::stoi(odp) == iloscLiterDoOdkrycia){
@@ -142,6 +142,12 @@ int main(int argc, char ** argv){
     sockaddr_in serverAddr{.sin_family=AF_INET, .sin_port=htons((short)port), .sin_addr={INADDR_ANY}};
     int res = bind(servFd, (sockaddr*) &serverAddr, sizeof(serverAddr));
     if(res) error(1, errno, "bind failed");
+
+    /*struct sigaction act;
+    act.sa_handler = SIG_IGN;
+    act.sa_flags = SA_RESTART;
+
+    sigaction(SIGPIPE, &act, NULL);*/
     
     res = listen(servFd, 1);
     if(res) error(1, errno, "listen failed");
